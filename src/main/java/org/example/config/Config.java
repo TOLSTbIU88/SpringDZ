91cff976cf4809a5a76e021208f8f8d2d8af7e6e
TOLSTbIU88 <gamer-9600@yandex.ru>
2022-11-23 21:08:47 +0300

    start

--- src/main/java/org/example/config/Config.java
+++ src/main/java/org/example/config/Config.java
@@ -1,8 +1,42 @@
 package org.example.config;
 
+import org.example.warehouse.Worker;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class Config {
+
+    @Bean
+    @Scope("prototype")
+    public Worker Mishka() {
+        return new Worker() {
+            @Override
+            public void isTired() {
+
+            }
+
+            @Override
+            public void addProduct() {
+
+            }
+        };
+    }
+    @Bean
+    @Scope("prototype")
+    public Worker Zelya() {
+        return new Worker() {
+            @Override
+            public void isTired() {
+
+            }
+
+            @Override
+            public void addProduct() {
+
+            }
+        };
+    }
 
 }
