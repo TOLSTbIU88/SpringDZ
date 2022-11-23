15af8f2c9c3a5081172de89192933a2b9f035bca
TOLSTbIU88 <gamer-9600@yandex.ru>
2022-11-23 21:18:43 +0300

    start

---
+++ src/main/java/org/example/Buyer.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.warehouse.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Buyer {
+
+    private List<Product> products=new ArrayList<>();
+}
---
+++ src/main/java/org/example/Reshar.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class Reshar extends Buyer {
+
+}
